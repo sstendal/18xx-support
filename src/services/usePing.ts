1b0278ea4300886bd72af8d4b4e32d81c459ff97
Sigurd Stendal <sigurd.stendal@fixrate.no>
2025-01-12 13:51:35 +0100

    Added ping - fixed problem with useEffect dependencies

--- src/services/usePing.ts
+++ src/services/usePing.ts
@@ -6,7 +6,7 @@ const REQUEST_TIMEOUT = 5000
 export const usePing = (sessionId: string) => {
     const timeoutRef = useRef<number>()
 
-    const sendPing = useCallback(() => async () => {
+    const sendPing = useCallback(async () => {
         const controller = new AbortController()
         const timeoutId = setTimeout(() => controller.abort(), REQUEST_TIMEOUT)
 
