4184a5b6c63b1f9d2b448b572edd0dd82ed9fa19
Sigurd Stendal <sigurd.stendal@fixrate.no>
2025-01-19 09:15:03 +0100

    Fixed sentry integration

--- src/index.tsx
+++ src/index.tsx
@@ -41,10 +41,20 @@ store.dispatch(listenToKeyEvents())
 
 // Using sentry to discover runtime errors in browsers
 Sentry.init({
-    dsn: "https://0d29e3571ce64cf9bcfbc2adaee937a8@o1187137.ingest.sentry.io/6307069",
-    integrations: [new BrowserTracing()],
-    tracesSampleRate: 1.0,
-});
+    dsn: "https://154043590edfb2252e4ca7f7e936e937@o1187137.ingest.us.sentry.io/4508669311123456",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: [/^https:\/\/18xx-support\.sstendal\.io/, /https:\/\/18xx-support\.netlify\.app/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
+
 
 
 ReactDOM.render(
